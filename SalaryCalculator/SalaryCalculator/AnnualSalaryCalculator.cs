41a38c5fdcbb4acf77b93194af6f0dff5940a33a
ybekardev <y_bekar@hotmail.com>
2021-05-17 21:22:36 -0500

    testing

---
+++ SalaryCalculator/SalaryCalculator/AnnualSalaryCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace AnnualSalaryCalculator
+{
+    public class SalaryCalculator
+    {
+        public decimal GetAnualSalary(decimal hourlyWage)
+        {
+            const int HoursInYear = 2080;
+            decimal annualSalary = hourlyWage * HoursInYear;
+            Console.WriteLine($"The annual salary is: {annualSalary}");
+            return annualSalary;
+        }
+
+        public decimal GetHourlyWage(decimal annualSalary)
+        {
+            const int HoursInYear = 2080;
+            decimal hourlyWage = annualSalary / HoursInYear;
+            Console.WriteLine($"The hourly wage is: {hourlyWage}");
+            return hourlyWage;
+        }
+    }
+}
---
+++ SalaryCalculator/SalaryCalculator/CalculatorTest.cs
@@ -0,0 +1,25 @@
+using AnnualSalaryCalculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void AnnualSalary()
+        {
+            SalaryCalculator sc = new SalaryCalculator();
+            decimal annualSalary = sc.GetAnualSalary(50);
+            Assert.AreEqual(104000, annualSalary);
+        }
+
+        [TestMethod]
+        public void HourlyWage()
+        {
+            SalaryCalculator sc = new SalaryCalculator();
+            decimal hourlyWage = sc.GetHourlyWage(52000);
+            Assert.AreEqual(25, hourlyWage);
+        }
+    }
+}
